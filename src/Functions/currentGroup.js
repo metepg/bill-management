45f27312afcb3ffb2f64244512f5b73388434ea1
unknown <meteg66@gmail.com>
2020-11-20 21:01:52 +0000

    Added documentation part3

--- src/Functions/currentGroup.js
+++ src/Functions/currentGroup.js
@@ -3,6 +3,7 @@ import load from './loadFromLocalStorage';
  * From localStorage:
  * Get current groups data matching the group name
  * @param {string} name - Group name to match
+ * @returns {Object} - Eg. { groupName: 'Meters', userNames: ['John','Mike','Helen'] }
  */
 function currentGroup(name) {
   const groups = load('savedGroups');
