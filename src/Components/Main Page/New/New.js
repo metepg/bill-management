895e68542183de4a830ed1fd43029a482659fa7c
unknown <meteg66@gmail.com>
2020-12-07 13:30:03 +0200

    Changed max users to 7

--- src/Components/Main Page/New/New.js
+++ src/Components/Main Page/New/New.js
@@ -41,7 +41,6 @@ function New(appProps) {
             <option value={5}>5</option>
             <option value={6}>6</option>
             <option value={7}>7</option>
-            <option value={8}>8</option>
           </Form.Control>
         </Form.Group>
         <Form.Group controlId="users">
